ba54476e235bf8538a67f3893df5c5f799ad4308
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-04-21 14:28:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,31 +1,43 @@
+// Your code here
 class Polygon {
-  constructor(sides) {
-    this.sides = sides
-    this.count = this.sides.length
+  constructor(arrayofintegers) {
+    this.arrayofintegers = arrayofintegers
+    this.count = this.arrayofintegers.length
   }
 
   get countSides() {
-    return this.sides.length
+    return this.arrayofintegers.length;
   }
 
   get perimeter() {
-    if (Array.isArray(this.sides)) return;
+    if (!Array.isArray(this.arrayofintegers)) return;
     let sum = 0;
-    for (var int of this.sides) {
-      sum += int
+    for ( let i = 0; i < this.arrayofintegers.length; i++) {
+      sum += this.arrayofintegers[i];
     }
-    return sum
+    return sum;
   }
 }
 
 class Triangle extends Polygon {
-
   get isValid() {
-    if (Array.isArray(this.sides)) return;
-    if (this.count == 3) return;
-    let side1 = this.sides[0]
-    let side2 = this.sides[1]
-    let side3 = this.sides[2]
-    return ((side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1))
+    // checks if the given 3 sides for a triangle is valid.
+    if (!Array.isArray(this.arrayofintegers)) return;
+      if (this.count !== 3) return;
+      let a = this.arrayofintegers[0]
+      let b = this.arrayofintegers[1]
+      let c = this.arrayofintegers[2]
+      return ((a + b > c) && (b + C > a) && (c + a > b))
+    // the sume of the lengths of any two sides of a tiangle is greatert han the length of the thrid side. if you take the three sides of a trianald and add them in paris, the sum is greatino than (not equal to) the thrid side. If that is not true, then it is not possible to construct a trianld w ith the given side lengths.
+  }
+}
+
+class Square extends Polygon {
+  get inValid() {
+    if (Array.isArray) return;
+      let a = this.arrayofintegers[0]
+      let b = this.arrayofintegers[1]
+      let c = this.arrayofintegers[2]
+      let d = this.arrayofintegers[3]
   }
 }
