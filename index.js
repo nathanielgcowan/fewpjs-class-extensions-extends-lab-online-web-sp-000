1943051b8892e9be620b26ee4e12e4a7f4041b01
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-04-21 14:27:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ class Polygon {
 class Triangle extends Polygon {
 
   get isValid() {
-    if (!Array.isArray(this.sides)) return;
-    if (this.count !== 3) return;
+    if (Array.isArray(this.sides)) return;
+    if (this.count == 3) return;
     let side1 = this.sides[0]
     let side2 = this.sides[1]
     let side3 = this.sides[2]
