0f1af3951e26f9987191f8b970a3c123c4f31de9
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-04-21 14:20:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,50 +1,43 @@
+// Your code here
 class Polygon {
   constructor(arrayofintegers) {
-    this.sides = arrayofintegers
+    this.arrayofintegers = arrayofintegers
     this.count = this.arrayofintegers.length
   }
 
   get countSides() {
-    return this.arrayofintegers.length
+    return this.arrayofintegers.length;
   }
 
   get perimeter() {
-    if (!Array.isArray(this.arrayofintegers)) return;
     let sum = 0;
-    for (var int of this.arrayofintegers) {
-      sum += int
+    for ( let i = 0; i < this.arrayofintegers.length; i++) {
+      sum += this.arrayofintegers[i];
     }
-    return sum
+    return sum;
   }
 }
 
 class Triangle extends Polygon {
-
   get isValid() {
+    // checks if the given 3 sides for a triangle is valid.
     if (!Array.isArray(this.arrayofintegers)) return;
-    if (this.count !== 3) return;
-    let side1 = this.arrayofintegers[0]
-    let side2 = this.arrayofintegers[1]
-    let side3 = this.arrayofintegers[2]
-    return ((side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1))
+      if (this.count !== 3) return;
+      let a = this.arrayofintegers[0]
+      let b = this.arrayofintegers[1]
+      let c = this.arrayofintegers[2]
+      return ((a + b > c) && (b + C > a) && (c + a > b))
+    // the sume of the lengths of any two sides of a tiangle is greatert han the length of the thrid side. if you take the three sides of a trianald and add them in paris, the sum is greatino than (not equal to) the thrid side. If that is not true, then it is not possible to construct a trianld w ith the given side lengths.
+
   }
 }
 
 class Square extends Polygon {
-
-  get isValid() {
-    if (!Array.isArray(this.arrayofintegers)) return;
-    if (this.count !== 4) return;
-    let side1 = this.arrayofintegers[0]
-    let side2 = this.arrayofintegers[1]
-    let side3 = this.arrayofintegers[2]
-    let side4 = this.arrayofintegers[3]
-    return ((side1 === side2) && (side1 === side3) && (side1 === side4))
-  }
-
-  get area() {
-    if (!Array.isArray(this.sides)) return;
-    if (this.count !== 4) return;
-    return this.sides[0] * this.sides[0]
+  get inValid() {
+    if (Array.isArray) return;
+      let a = this.arrayofintegers[0]
+      let b = this.arrayofintegers[1]
+      let c = this.arrayofintegers[2]
+      let d = this.arrayofintegers[3]
   }
 }
