76f3f834762f73bc1c657dcc7780d22c43f6ef7f
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-04-21 14:00:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+class Polygon {
+  constructor(sideLength) {
+    this.sideLength = sideLength;
+  }
+
+  get countSides() {
+    return this.sideLength.length;
+  }
+
+  get perimeter() {
+    let sum = 0;
+    for ( let i = 0; i < this.sideLength.length; i++) {
+      sum += this.sideLength[i];
+    }
+    return sum;
+  }
+}
+
+class Triangle extends Polygon {
+
+}
+
+class Square extends Polygon {
+
+}
